8f289450942fd6a4791fb21d2431cbb4e6359b45
Willian Barreto <willianblopes@gmail.com>
2020-01-07 00:26:32 -0300

    completo
    completo

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,21 @@
+// index, show , store, update , destroy
+
+const User = require('../models/User');
+
+module.exports = {
+  async store(req,res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if(!user) {
+
+            user = await User.create({ email });
+
+        }
+
+        //const 
+
+        return res.json(user);
+    }
+};
---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app =  express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-uy0ua.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
+
+//GET, POST, PUT, DELETE
+
+//req.query = Acessar query params (para filtros)
+//req.params = Acessar route paramns (para edição, delete)
+//req.body = Acessar corpo da requisicao (Para criação, edição)
+//dev
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);  
+
+server.listen(3333);
